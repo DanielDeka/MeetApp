f9fabc48c9b76805b9bb55b03f7af9b36589ede4
Daniel Kurniawan <dekaslr8@gmail.com>
2019-11-25 21:19:35 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,11 @@ Route::prefix('/admin')->group(function () {
 
 Route::prefix('/pengguna')->group(function () {
     Route::get('', 'PenggunaController@index');
+    Route::get('/mitra/{id}', 'PenggunaController@mitra');
+    Route::get('/mitra/{id}/booking', 'PenggunaController@booking');
+    Route::get('status-pemesanan', 'PenggunaController@statusPemesanan');
+    Route::post('/submit-booking', 'PenggunaController@buatPemesanan');
+    Route::post('/submit-verifikasi-booking', 'PenggunaController@verifikasiPemesanan');
 });
 
 
@@ -35,10 +40,4 @@ Route::prefix('/mitra')->group(function () {
     Route::get('/mitra', 'MitraController@mitra');
     Route::get('/edit','MitraController@edit');
     Route::get('/konfirmasi', 'MitraController@konfirmasi');
-    Route::get('/mitra/{id}', 'PenggunaController@mitra');
-    Route::get('/mitra/{id}/booking', 'PenggunaController@booking');
-    Route::get('status-pemesanan', 'PenggunaController@statusPemesanan');
-
-    Route::post('/submit-booking', 'PenggunaController@buatPemesanan');
-    Route::post('/submit-verifikasi-booking', 'PenggunaController@verifikasiPemesanan');
 });
